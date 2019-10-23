cc3aba116b22113c3ac086a49128bdaf861ea74e
Administrator <15625121904@163.com>
2019-10-23 20:43:42 +0800

    Initial commit

---
+++ turtle_teach1.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+def main():
+    turtle.forward(200)
+    turtle.right(144)
+    turtle.forward(200)
+    turtle.right(144)
+    turtle.forward(200)
+    turtle.right(144)
+    turtle.forward(200)
+    turtle.right(144)
+    turtle.forward(200)
+    turtle.right(144)
+    turtle.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
